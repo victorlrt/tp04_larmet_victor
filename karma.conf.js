ce905e5b520b20dce01ccdd50d63852fe38939a4
Victor Larmet <victor.larmet@gmail.com>
2022-11-10 13:59:14 +0100

    wip heroku

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tp03_larmet_victor'),
+      dir: require('path').join(__dirname, './coverage/tp04_larmet_victor'),
       subdir: '.',
       reporters: [
         { type: 'html' },
